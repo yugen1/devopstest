f4cb443ca1435e7f32adc251dc20961e217787d9
Yogi <Yogi@DESKTOP-0HHCAHO>
2025-10-18 17:04:34 -0400

    updated

--- my-webapp/src/main/java/test/Test.java
+++ my-webapp/src/main/java/test/Test.java
@@ -8,8 +8,8 @@ package test;
  */
 public class Test {
 	
-	public String getGreeting(String name) {
-        return "Hello, " + name + "!";
+	public String getGreeting(String Myname) {
+        return "Hello!!!!!!!!!!!!!!!, " + Myname + "!";
     }
 	
 
